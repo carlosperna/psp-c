e9809c5514049eed542143c3298d8b366b99a6cb
carlos <170070@iessierradeguara.com>
2021-11-08 11:08:36 +0100

    08/11/2021

---
+++ ejercicioCompleto/funciones.h
@@ -0,0 +1,7 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+    unsigned int int_aleatorio(unsigned int min,unsigned int max);
+
+#endif
+
--- senyales/age_of_empires.c
+++ senyales/age_of_empires.c
@@ -1,56 +1,54 @@
+#include <stdio.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <sys/types.h>
-#include <sys/wait.h>
 #include <signal.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include <sys/wait.h>
 #include <stdbool.h>
 
 void tratarSenyal(int cod_senyal);
 
-pid_t minero;
+pid_t pidHijo; //variable global para poder ser utilizada
+                //desde el main y tambien desde la función.
 bool final = false;
-
 
 int main(int argc, char const *argv[])
 {
-    switch (minero = fork())
+    pidHijo = fork();
+    switch (pidHijo)
     {
-    case 1:
+    case -1:
         exit(1);
-        break;
-
-    case 0: //Minero
+        break; 
+    case 0: //hijo
         signal(SIGUSR2, tratarSenyal);
-        signal(SIGINT, SIG_IGN); //Ignoramos señal
-        while (!final)
-        {
-            printf("[Minero] Esperando 5s..\n");
+        signal(SIGINT, SIG_IGN); //IGNORAMOS SEÑAL CTRL-C
+        printf("[Hijo %d]: Esperando señal\n", getpid());
+        while(!final){
+            printf("[Hijo]: Esperando 5 segundos...\n");
             sleep(5);
         }
         break;
-
-    default: //Padre
+    default: //padre
         signal(SIGINT, tratarSenyal);
-        printf("[Padre] Esperando señal..\n");
+        printf("[Padre %d]: Esperando señal\n", getpid());
         pause();
-        wait(NULL);
+        wait(NULL); //esperar al hijo
     }
+
+
     return 0;
 }
 
-void tratarSenyal(int cod_senyal)
-{
-    if (cod_senyal == SIGINT)
-    {
-        printf("[Proceso %d] Señal recibida %d\n", getpid, cod_senyal);
-        printf("Enviando señal al hijo con PID %d en 3 segundos\n", minero);
+void tratarSenyal(int cod_senyal){
+    if (cod_senyal == SIGINT){
+        printf("[Proceso %d]: recibida señal %d\n", getpid(), cod_senyal);
+        printf("Enviando señal al hijo con pid %d en 3 segundos\n", pidHijo);
         sleep(3);
-        kill(minero, SIGUSR2);
-    }
-    else if (cod_senyal == SIGUSR2)
-    {
-        printf("[Proceso %d] Señal recibida %d\n", getpid, cod_senyal);
-        final = true;
+        kill(pidHijo, SIGUSR2);
+    }else if(cod_senyal == SIGUSR2){
+        printf("[Proceso %d]: recibida señal %d\n", getpid(), cod_senyal);
+        final=true;        
     }
 }
